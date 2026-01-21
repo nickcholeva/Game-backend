3d57cb799e1f44f2cf06fdf77964f4842bd8dc5e
nickcholeva <nickcholeva@gmail.com>
2026-01-21 17:50:48 +0100

    Update app.py

--- app.py
+++ app.py
@@ -17,5 +17,8 @@ def remove_item():
         data["codes"].remove(item)
     return jsonify(data)
 
+import os
+
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
